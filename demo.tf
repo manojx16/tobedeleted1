6dd0430072271d376a09559ed873dc4cba649889
Manoj Kumar <153157931+manojx16@users.noreply.github.com>
2025-04-11 21:08:30 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -15,10 +15,6 @@ resource "aws_ecr_repository" "my_ecr_repo" {
   name                 = "ecr" # Change this to your repo name
   image_tag_mutability = "MUTABLE"     # or "IMMUTABLE"
 
-provider "aws" {
-  region = "us-east-1" # Change as needed
-}
-
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "manojgithubactions3" # Replace with a globally unique name
   force_destroy = true                  # Allows deletion even if objects exist
